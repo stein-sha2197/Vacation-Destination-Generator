380e3d0a3641dd4fbbffbef0531336ba2fec873e
stein-sha2197 <95261695+stein-sha2197@users.noreply.github.com>
2023-01-15 15:02:25 -0700

    Update vacay_destination.py

--- vacay_destination.py
+++ vacay_destination.py
@@ -1,4 +1,4 @@
-'''
+"""
 Vacation Destination Random Generator
 Sharon Steinke
 December 19, 2021
@@ -7,42 +7,32 @@ A simple random generator specifically for choosing vacation destinations.
 To use: enter destinations, one per line, in plain txt document. Make sure there are no 
 new lines at the end of the list. call vacay_generator with location of your file as
 a parameter. 
-'''
+"""
 import random
 
 def get_vacay_lyst(vacay_file):
-    '''reads a file and stores destinations in a list'''
+    """reads a file and stores destinations in a list"""
     with open(vacay_file, "r") as vacays:
         vacay_lyst = []
         for line in vacays:
-            lyst = line.strip().split('\n')
-            vacay_lyst.append(lyst[0])
+          vacay_lyst.append(line)
         return vacay_lyst
-
-
+        
 def vacay_generator(vacay_file):
-    '''picks a random destination from list'''
+    """picks a random destination from list"""
     vacays = get_vacay_lyst(vacay_file)
-    print('Welcome to Vacation Destination Generator')
-    start_stop = ''
-    while start_stop.upper() != 'N':
-        start_stop = input('Generate  a new destination? (Y/N): ')
-        if start_stop.upper() == 'Y':
-            new_destination = random.choice(vacays)
-            print(f'Pack your bags, you are going to: {new_destination}')
-            start_over = input('Would you like to play again? (Y/N): ')
-            if start_over.upper() == 'Y':
-                return vacay_generator(vacay_file)   
-            if start_over.upper() == 'N':
-                good_bye  = print(f'Goodbye.')
-                return good_bye
-        if start_stop.upper() == 'N':
-            good_bye  = print(f'Goodbye.')
-            return good_bye
+    print("Welcome to Vacation Destination Generator\n")
+    start_stop = input("Generate  a new destination? (Y/Q): ")
+    while start_stop.upper() != 'Q':
+        new_destination = random.choice(vacays)
+        print(f"\nPack your bags, you are going to: {new_destination}")
+        start_stop = input("Generate a new destination? (Y/Q): ")
+          
+    good_bye  = print("\nGoodbye.")
+    return good_bye
     
 def main():
     vacay_generator('vacay_dest.txt')
-
 
 if __name__ == "__main__":
     
